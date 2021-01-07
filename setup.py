f05f69310845bbfff2fa2f2d87e3c74d77e1a913
seblin <Seb_Linke@arcor.de>
2021-01-07 20:51:11 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import simplecol
+
+def main():
+    setup(
+        name="simplecol",
+        description="Easy-to-use columnizer for your shell",
+        version=simplecol.__version__,
+        license=shcol.__license__,
+        author=shcol.__author__,
+        author_email="Seb_Linke@arcor.de",
+        url="https://github.com/seblin/simplecol",
+        packages=["simplecol"],
+        platforms="any",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Intended Audience :: End Users/Desktop",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
+            "Topic :: System :: Shells",
+        ],
+    )
+
+if __name__ == "__main__":
+    main()
